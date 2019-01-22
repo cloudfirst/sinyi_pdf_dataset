c017a72257a6096e025925c342d80adb973940d2
jason liu <jason.liu@sinobot.com.cn>
2019-01-22 17:41:06 +0800

    calamari-regression

---
+++ calamari-regression/regression_test/file_op.py
@@ -0,0 +1,110 @@
+import os
+import sys
+import numpy as np
+
+class TestData():
+    def  __init__(self):
+        self.upper_path = ''
+
+        self.upper_obj = None
+        self.child_obj = []
+
+        self.full_path = ''
+        self.relative_path = ''
+        self.level = 0
+
+        self.files = []
+        self.total_num = 0
+        self.error_num = 0
+
+def write_to_file(filepath, content):
+    f = open(filepath, 'a+', encoding='utf-8')
+    f.write(content)
+    f.close
+
+
+def get_gt(path):
+    gt = []
+
+    for line in open(path, encoding='utf-8'):
+        gt.append(line.strip('\n'))
+    
+    return gt
+
+def find_upper_and_child(test_data_list):
+    for test_data in test_data_list:
+        for data in test_data_list:
+            if test_data.upper_path == data.full_path:
+                test_data.upper_obj = data
+            
+            if data.upper_path == test_data.full_path:
+                test_data.child_obj.append(data)
+    
+def gen_test_data(root_path):
+    test_data_list = []
+    file_counter = 0
+    pre_level = 0
+    level = 0
+
+    for root, dirs, files in os.walk(root_path, topdown = False): 
+        if root != root_path:
+            test_data = TestData()
+
+            test_data.upper_path = os.path.split(root)[0]
+            test_data.full_path = root
+            test_data.relative_path = root[len(root_path):]
+            test_data.level = test_data.relative_path.count('/')
+
+            # the first time coming in this loop
+            if not len(test_data_list):
+                pre_level = test_data.level 
+                level_total_num = np.zeros((pre_level,), dtype=np.int)
+
+            if level < test_data.level:
+                level = test_data.level
+
+            for f in files:
+                filepath, tempfilename = os.path.split(f);
+                shotname, extension = os.path.splitext(tempfilename);
+
+                file_dict = dict()
+
+                if extension.lower()  == '.pdf':
+                    corresponding_gt_file = os.path.join(root, shotname + '.gt.txt')
+                    if os.path.exists(corresponding_gt_file):
+                        file_dict['pdf'] = os.path.join(root, tempfilename)
+                        file_dict['gt'] = corresponding_gt_file
+                        test_data.files.append(file_dict)
+
+                        test_data.total_num = test_data.total_num + 1
+                    else:
+                        raise Exception("pdf %s does not have corresponding gt.txt file, check your training data file!" % shotname)
+               
+            index = test_data.level - 1
+
+            if pre_level <= test_data.level:
+                level_total_num[index] += test_data.total_num
+            elif pre_level > test_data.level:
+                test_data.total_num = np.sum(level_total_num[index + 1:])
+                level_total_num[index] += test_data.total_num
+                level_total_num[index + 1:] = 0
+
+            if test_data.level == 1:
+                level_total_num[:] = 0
+
+            #print("path: %s, pre_level: %d, level: %d, total_num: %d" % (root, pre_level, test_data.level, test_data.total_num))
+            #print(level_total_num)
+
+            test_data_list.append(test_data)
+            pre_level = test_data.level
+       
+    return test_data_list, level
+
+#root = '/home/luhya/Documents/src/xy_train_data/calamari-regression/test_data'
+#test_data_list, level = gen_test_data(root)
+#print(level)
+#print(test_data_list)
+
+#path = '/home/luhya/Documents/src/xy_train_data/calamari-regression/test_data/number/handwriting/NH50620_107BC418850PIC24D6EAD66180543FF975AE9B28BEF.gt.txt'
+#gt = get_gt(path)
+#print(gt)
---
+++ calamari-regression/regression_test/regression_test.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+# -*- encoding: utf8 -*-
+
+import os
+import sys
+import argparse
+
+from file_op import TestData, gen_test_data, get_gt, write_to_file
+
+from sinobotocr.cv2_helper import *
+from sinobotocr.cv2_helper2 import *
+from sinobotocr.my_pdf2img import *
+
+logger = get_my_logger()
+db_name = 'test.db'
+
+def get_statistic_result(data_list, result_path):
+
+    data_list.reverse()
+    for d in data_list:
+        if d.total_num:
+            percentage = float(d.total_num - d.error_num)/d.total_num
+        else:
+            percentage = 0.0
+	
+        content = "%s/%s %s%% %s\n" % (str(d.error_num), str(d.total_num), str(percentage*100), d.relative_path)
+        #content = 'total_num: ' + str(d.total_num) + ' error_num: ' + str(d.error_num) + ' percentage: ' + str(percentage) + ' ' + d.relative_path + '\n'
+        #content = str(percentage) + ' ' + d.relative_path + '\n'
+        write_to_file(result_path, content)
+
+
+def rec_img(path):
+
+    logger.error("Get image from pdf from %s" % path)
+    dest_file = getImageFromPDF(path)
+    filename, file_extension = os.path.splitext(path)
+    
+    # Step 1: 图像预处理
+    logger.error("step_1_pre_processing_image ...")
+    orig, canny = step_1_pre_processing_image(dest_file)
+    
+    # Step 2: 定位并提取表格部分
+    logger.error("step_2_location_table ...")
+    table       = step_2_location_table(orig, canny)
+    # save_processed_image(table,   "/tmp/table_"  + filename + ".tif")
+    
+    # Step 3: 提取表格中每一行的文本
+    logger.error("step_3_find_text_lines ...")
+    text_blocks = step_3_find_text_lines(table, filename)
+    
+    # Step 4: 识别表格中每一行的文本，并查找关键字极其取值
+    logger.error("step_4_read_keyword_and_value ...")
+    areas, ztgz = step_4_read_keyword_and_value_v2(text_blocks, filename)
+    
+    ret = {}
+    ret['filename']  = path
+    if len(areas) == 2:
+        ret['heji1'] = areas[0]
+        ret['heji2'] = areas[1]
+    else:
+        ret['heji1'] = "0.0"
+        ret['heji2'] = "0.0"
+    
+    ret['ztgz']      = ztgz
+    ret['confident'] = 0.8
+    ret['Status']    = "OK"
+    ret['ErrDesc']   = ""
+    
+    # print(ret['filename'], " # ", ret['heji1'], " / ", ret['heji2'] ,"ztgz:",ztgz)
+    # logger.error(" --- wgb:  %s, %s / %s, ztgz : %s" % (str(ret['filename']), str(ret['heji1']), str(ret['heji2']), ztgz))
+
+    return ret
+
+def get_result(test_data, gt, ret, flag):
+    if flag == "all":
+        if gt[0] == ret['ztgz']  and float(gt[1]) == float(ret['heji1']) and float(gt[2]) == float(ret['heji2']):
+            return True
+        else:
+            return False
+    if flag == "heji":
+        if float(gt[1]) == float(ret['heji1']) and float(gt[2]) == float(ret['heji2']):
+            return True
+        else:
+            return False
+    if flag == "ztgz":
+        if gt[0] == ret['ztgz']:
+            return True
+        else:
+            return False
+
+'''
+def calc_error_num(test_data_list):
+    error_num = 0
+
+    for test_data in test_data_list:
+        if test_data.child_path:
+           for tes
+'''
+
+def setup_train_args(parser):
+    parser.add_argument("-r", "--root", default=False, help='root path of data')
+    parser.add_argument("-t", "--result", default=False, help='result path')
+    # all, heji, and ztgz
+    parser.add_argument("-f", "--flag", default="heji", help="result compare flag")
+
+def usage():
+    print("Usage: ")
+    print(" python regression.py --root <pdf directory> --result <result file full path> --flag <compare flag> ")
+    print("   pdf directory, say, /tmp/test")
+    print("   result file,   say, /tmp/result")
+    print("   compare flag,  say, one of [all, heji, ztgz]")
+    
+def main():
+    parser = argparse.ArgumentParser()
+    setup_train_args(parser)
+    args = parser.parse_args()
+
+    if args.root == False or args.result == False:
+        usage()
+        return
+
+    test_data_list, level = gen_test_data(args.root)
+
+    for test_data in test_data_list:
+
+        if test_data == test_data_list[0]:
+            pre_level = test_data.level
+            level_error_num = np.zeros((pre_level,), dtype=np.int)
+
+        if test_data.files:
+            # test_data is of type TestData
+            print("-----path: %s------" % (test_data.relative_path))
+            for f in test_data.files:
+
+                path = f['pdf']
+                gt_path = f['gt']
+                gt = get_gt(gt_path)
+
+                try:
+                    ret = rec_img(path)
+                    if not get_result(test_data, gt, ret, args.flag):
+                        test_data.error_num += 1
+                        write_to_file(args.result+'_files', path + '\n')
+                        content_rec = 'rec: ' + '[' + ret['ztgz'] + ',' + ret['heji1'] + ',' + ret['heji2'] + ']'
+                        content_gt = '; gt: ' + '[' + gt[0] + ',' + gt[1] + ',' + gt[2] + ']\n'
+                        
+                        write_to_file(args.result+'_files', content_rec + content_gt)
+                        write_to_file(args.result+'_files', '\n')
+
+                except Exception as e:
+                    print(str(e))
+
+        ######## calc error_num
+        index = test_data.level - 1
+        if pre_level <= test_data.level:
+            level_error_num[index] += test_data.error_num
+        elif pre_level > test_data.level:
+            test_data.error_num = np.sum(level_error_num[index + 1:])
+            level_error_num[index] += test_data.error_num
+            level_error_num[index + 1:] = 0
+
+        if test_data.level == 1:
+            level_error_num[:] = 0
+
+        #print("path: %s, pre_level: %d, level: %d, error_num: %d" % (root, pre_level, test_data.level, test_data.error_num))
+        pre_level = test_data.level
+        ######## calc error_num
+
+    get_statistic_result(test_data_list, args.result)
+
+if __name__ == '__main__':
+    main()
+
